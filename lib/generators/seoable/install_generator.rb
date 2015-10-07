ef129ce81cd0da7a47beef9b6dec3b72598c2a49
Viktor Fonic <viktor.fonic@gmail.com>
2015-10-07 16:31:56 +0100

    Create install rails generator

---
+++ lib/generators/seoable/install_generator.rb
@@ -0,0 +1,22 @@
+require 'rails/generators/active_record'
+
+module Seoable
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include ::Rails::Generators::Migration
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      desc 'Installs Seoable and generates the necessary migrations'
+
+      def create_migrations
+        migration_template 'create_seo_details.rb',
+                           'db/migrate/create_seo_details.rb'
+      end
+
+      def self.next_migration_number(dirname)
+        ActiveRecord::Generators::Base.next_migration_number(dirname)
+      end
+    end
+  end
+end
