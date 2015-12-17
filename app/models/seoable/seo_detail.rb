7f3d64f4882a7356a1fe6c3c07232f500ce61cbd
Felipe Rodrigues <felipe@crafters.com.br>
2015-12-17 17:23:09 -0200

    Adding the redirects to the seo_detail

--- app/models/seoable/seo_detail.rb
+++ app/models/seoable/seo_detail.rb
@@ -4,10 +4,21 @@ module Seoable
     @@permitted_attribute_names = [:meta_title, :meta_description, :slug]
 
     belongs_to :seoable, polymorphic: true
+    has_many :redirects, class_name: 'SeoDetailRedirect'
+
+    after_update :create_redirect_if_needed
 
     validates :meta_title, presence: true, uniqueness: { scope: :seoable_type }
     validates :slug,
               uniqueness: { scope: :seoable_type },
               format: /\A[a-z0-9-]+\z/
+
+    scope :with_redirects, -> (model, slug){ includes(:redirects).where('seoable_type = ? AND (seo_detail_redirects.old_slug = ? OR slug = ?)', model, slug, slug).references(:redirects) }
+
+    private
+
+    def create_redirect_if_needed
+      self.redirects.create(old_slug: self.slug) if self.slug_changed?
+    end
   end
 end
--- spec/models/seo_detail_spec.rb
+++ spec/models/seo_detail_spec.rb
@@ -14,6 +14,7 @@ module Seoable
 
     context 'relations' do
       it { is_expected.to belong_to(:seoable) }
+      it { is_expected.to have_many(:redirects).class_name('SeoDetailRedirect') }
     end
 
     context '.permitted_attribute_names' do
@@ -21,5 +22,54 @@ module Seoable
         expect(SeoDetail.permitted_attribute_names).to be_a(Array)
       end
     end
+
+    describe '.with_redirects' do
+      context 'when redirect exists' do
+        it 'returns the seo detail on the redirect' do
+          seo_detail = create(:seo_detail, slug: 'robin', seoable_type: 'post')
+          create(:seo_detail_redirects, seo_detail: seo_detail, old_slug: 'blake')
+          expect(SeoDetail.with_redirects('post', 'blake').first).to eq seo_detail
+        end
+
+        it 'returns the directly the seo detail of the slug' do
+          seo_detail = create(:seo_detail, slug: 'robin', seoable_type: 'post')
+          create(:seo_detail_redirects, seo_detail: seo_detail, old_slug: 'blake')
+          expect(SeoDetail.with_redirects('post', 'robin').first).to eq seo_detail
+        end
+      end
+
+      context 'when redirect does not exists' do
+        it 'returns the seo detail for the slug' do
+          seo_detail = create(:seo_detail, slug: 'robin', seoable_type: 'post')
+          expect(SeoDetail.with_redirects('post', 'robin').first).to eq seo_detail
+        end
+      end
+
+      context 'when there is no seo details for the slug' do
+        it 'returns nothing' do
+          create(:seo_detail, slug: 'robin', seoable_type: 'post')
+          expect(SeoDetail.with_redirects('post', 'blake')).to be_empty
+        end
+      end
+    end
+
+    context 'slug change' do
+      it 'creates a new redirect record' do
+        seo_detail = create(:seo_detail, slug: 'batman-belt')
+        expect {
+          seo_detail.update_attributes(slug: 'superman-cape')
+        }.to change { seo_detail.redirects.count }
+      end
+    end
+
+    context 'slug does not change' do
+      it 'creates a new redirect record' do
+        seo_detail = create(:seo_detail, slug: 'batman-belt')
+        expect {
+          seo_detail.update_attributes(meta_title: 'the dark night')
+        }.not_to change { seo_detail.redirects.count }
+      end
+    end
+
   end
 end
