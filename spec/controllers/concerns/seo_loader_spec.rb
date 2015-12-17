68839438aa077b2a708d565ded24d605b5c34a8b
Felipe Rodrigues <felipe@crafters.com.br>
2015-12-17 17:24:22 -0200

    Redirect to the new slug if the slug has changed.

--- spec/controllers/concerns/seo_loader_spec.rb
+++ spec/controllers/concerns/seo_loader_spec.rb
@@ -17,13 +17,28 @@ module Seoable
       before(:each) do
         allow(FakeController).to receive(:prepend_before_action)
         allow(Loader).to receive(:new).and_return(seo_detail_loader)
-        seo_loader.params = {}
+        seo_loader.params = {id: 'batman'}
       end
 
       it 'loads seo details from loader' do
-        expect(seo_detail_loader).to receive(:load)
-
+        allow(seo_detail_loader).to receive(:load).and_return(create(:seo_detail, slug: 'batman'))
         subject
+      end
+
+      context 'returned slug is different' do
+        before(:each) do
+          allow(FakeController).to receive(:prepend_before_action)
+          loader = double('loader')
+          allow(Loader).to receive(:new).and_return(loader)
+          allow(loader).to receive(:load).and_return(create(:seo_detail, slug: 'clark'))
+          seo_loader.params = {id: 'superman'}
+        end
+
+        it 'redirects to the new one' do
+          allow_any_instance_of(FakeController).to receive(:request).and_return(double('request', path: 'bio/superman/full'))
+          expect_any_instance_of(FakeController).to receive(:redirect_to).with('bio/clark/full')
+          seo_loader.load_seo_detail
+        end
       end
     end
   end
